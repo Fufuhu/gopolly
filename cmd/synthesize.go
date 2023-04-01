7d9bcc20c96d9edc4297ddc8bb3e9f53587f163c
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2023-04-01 23:27:09 +0900

    一旦作成済み

--- cmd/synthesize.go
+++ cmd/synthesize.go
@@ -4,9 +4,15 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"context"
+	"fmt"
+	"github.com/Fufuhu/gopolly/util/client/polly"
 	"github.com/Fufuhu/gopolly/util/logging"
+	polly2 "github.com/aws/aws-sdk-go-v2/service/polly"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
+	"io"
+	"os"
 )
 
 // synthesizeCmd represents the synthesize command
@@ -34,16 +40,79 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// synthesizeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	synthesizeCmd.Flags().String("file", "", "Transcript file to synthesize voice audio")
-	err := synthesizeCmd.MarkFlagRequired("file")
+	synthesizeCmd.Flags().StringVarP(&filePath, "filePath", "f", "", "Transcript file to synthesize voice audio")
+	err := synthesizeCmd.MarkFlagRequired("filePath")
+	if err != nil {
+		return
+	}
+	synthesizeCmd.Flags().StringVarP(&outputFilePath, "outputFilePath", "o", "", "Output file path")
+	err = synthesizeCmd.MarkFlagRequired("outputFilePath")
 	if err != nil {
 		return
 	}
 }
+
+var filePath string
+var outputFilePath string
 
 func synthesize(cmd *cobra.Command, args []string) {
 	logger := logging.GetLogger()
 	defer func(logger *zap.Logger) {
 		_ = logger.Sync()
 	}(logger)
+
+	logger.Info(fmt.Sprintf("file path is %s", filePath))
+
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		logger.Warn(err.Error())
+		os.Exit(1)
+	}
+	contentString := string(content)
+
+	logger.Info(contentString)
+
+	client, err := polly.GetPollyClient(&polly.ClientConfig{})
+	if err != nil {
+		logger.Warn(err.Error())
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+
+	synthesizeSpeechInput := &polly2.SynthesizeSpeechInput{
+		Text:         &contentString,
+		OutputFormat: "mp3",
+		Engine:       "standard",
+		LanguageCode: "ja-JP",
+		VoiceId:      "Mizuki",
+	}
+
+	synthesizeSpeechOutput, err := client.SynthesizeSpeech(ctx, synthesizeSpeechInput)
+	if err != nil {
+		logger.Warn(err.Error())
+		os.Exit(1)
+	}
+	defer func(AudioStream io.ReadCloser) {
+		_ = AudioStream.Close()
+	}(synthesizeSpeechOutput.AudioStream)
+
+	outputFile, err := os.Create(outputFilePath)
+	defer func(outputFile *os.File) {
+		_ = outputFile.Close()
+	}(outputFile)
+
+	if err != nil {
+		logger.Warn(err.Error())
+		os.Exit(1)
+	}
+
+	logger.Info(fmt.Sprintf("%d characters were synthesized", synthesizeSpeechOutput.RequestCharacters))
+	written, err := io.Copy(outputFile, synthesizeSpeechOutput.AudioStream)
+	if err != nil {
+		logger.Warn(err.Error())
+		return
+	}
+
+	logger.Info(fmt.Sprintf("%d bytes were written", written))
 }
