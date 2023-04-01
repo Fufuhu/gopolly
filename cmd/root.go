5c74169319d3ca1452ecaf1e5e2468ee049a113c
Ryoma Fujiwara <e238058r@hotmail.co.jp>
2023-04-01 18:01:15 +0900

    rootコマンドを設定

--- cmd/root.go
+++ cmd/root.go
@@ -12,13 +12,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gopolly",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "gopolly is a sample apolication to use polly",
+	Long:  `gopolly is sample application to synthesize voice audio`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
